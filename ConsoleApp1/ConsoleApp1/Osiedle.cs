46d2804a2c285817f4276686b56d708d6d46421f
g.krempa <g.krempa@krd.pl>
2017-07-17 07:20:08 +0200

    - dodanie paczki KRD.StyleCopRules
    - dodanie klasy Osiedla

---
+++ ConsoleApp1/ConsoleApp1/Osiedle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  class Osiedle
+  {
+    public List<Dom> _dom = new List<Dom>();
+    public string NazwaOsiedla;
+    public string NazwaUlicy;
+
+    public Osiedle(string nazwaOsiedla, string nazwaUlicy)
+    { 
+      NazwaOsiedla = nazwaOsiedla;
+      NazwaUlicy = nazwaUlicy;
+    }
+  }
+}
